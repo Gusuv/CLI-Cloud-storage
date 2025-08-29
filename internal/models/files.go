49c0299343f4dbc893e70a7521264edbf5c8f9ee
Gusuv <papikanartur3@gmail.com>
2025-08-29 19:34:04 +0300

    fix: fixed db struct

--- internal/models/files.go
+++ internal/models/files.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type File struct {
-	Id        int64     `gorm:"primary_key"`
-	UserId    int64     `gorm:"not null;index"`
-	User      User      `gorm:"foreignKey:UserId"`
-	Name      string    `gorm:"not null"`
-	Path      string    `gorm:"not null"`
-	Size      int64     `gorm:"not null"`
-	MimeType  string    `gorm:"size:100"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
+	Id       int64     `gorm:"primary_key"`
+	UserId   int64     `gorm:"not null;index"`
+	User     User      `gorm:"foreignKey:UserId"`
+	Name     string    `gorm:"not null"`
+	Path     string    `gorm:"not null"`
+	Size     int64     `gorm:"not null"`
+	MimeType string    `gorm:"size:100"`
+	Created  time.Time `gorm:"autoCreateTime"`
 }
