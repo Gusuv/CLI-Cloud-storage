97a574c7237eb9b67c375b319b779d3dd3a86c85
Gusuv <papikanartur3@gmail.com>
2025-09-03 13:59:03 +0300

    feat: password hash

---
+++ internal/sec/passwordHash.go
@@ -0,0 +1,22 @@
+package sec
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hash), nil
+}
+
+func CompareHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
