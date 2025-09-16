c0d78c263917e018925952d183f9f6dbb73e278d
Gusuv <papikanartur3@gmail.com>
2025-09-16 23:58:12 +0300

    feat: user creat

---
+++ internal/api/handlers/user_handler.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"cloud_storage/internal/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserHandler struct {
+	service *services.User
+}
+
+func NewUserHandler(service *services.User) *UserHandler {
+	return &UserHandler{service: service}
+}
+
+func (u *UserHandler) Register(c *gin.Context) {
+	var req UserReq
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect JSON"})
+		return
+	}
+
+	if err := u.service.Register(req.Username, req.Password, req.Email); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username is already taken"})
+		return
+
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully!"})
+
+}
+
+type UserReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
--- internal/api/router.go
+++ internal/api/router.go
@@ -1,13 +1,22 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"cloud_storage/internal/api/handlers"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func NewRouter() *gin.Engine {
+func NewRouter(u *handlers.UserHandler) *gin.Engine {
 	r := gin.Default()
 	r.Use(gin.Recovery())
 
 	v1 := r.Group("/user")
-	v1.POST("/register")
+	v1.POST("/register", u.Register)
+	v1.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello User",
+		})
+	})
 
 	return r
 
