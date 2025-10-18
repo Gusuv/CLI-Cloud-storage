51f9cb54c801b8fdff2e2c88f65745d9dda8d84a
Gusuv <papikanartur3@gmail.com>
2025-10-19 01:47:05 +0300

    refactor: hash compare function in singly file

---
+++ internal/security/compareHash.go
@@ -0,0 +1,11 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+func CompareHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
